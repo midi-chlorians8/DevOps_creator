8bd7402f964915c021454cdd06ad6b2d8d376bdd
Shutlersss <denis.korol.001@gmial.com>
2022-04-06 03:26:01 +0300

    update v0.1

--- DevOps_creator.py
+++ DevOps_creator.py
@@ -1,69 +1,36 @@
-#Python v3.9
-#source myvenv/bin/activate
-#python -m pip install pysimplegui
-# pyinstaller -F -w -i "C:\DevOps_create_new_project\icon.ico" DevOps_creator.py
-
-#import os
-#os.system('rik-i-morti.mp3')
-
-
 from folder_add import *
-"""
-import os
-
-# Функция проверяет не создана ли уже папка чтобы не бить ошибки
-def create_dir(dir):
-  if not os.path.exists(dir):
-    os.makedirs(dir)
-    print("Created Directory : ", dir)
-  else:
-    print("Directory already existed : ", dir)
-  return dir
-# Функция проверяет не создана ли уже папка чтобы не бить ошибки
-
-name = 'proj'
-
-def add_folder_in_project(main_folder_name):
-    create_dir(main_folder_name)
-
-    root_path =  main_folder_name + '/'
-
-    create_dir(root_path+"Creds")
-    f= open(root_path+"Creds/aws.txt","w+")
-
-    create_dir(root_path+"Creds/Db_Creds")
-    f= open(root_path+"Creds/Db_Creds/Db_creds.txt","w+")
-
-    create_dir(root_path+"Videos")
-    create_dir(root_path+"K8s")
-    create_dir(root_path+"Terraform")
-    create_dir(root_path+"Proposal")
-"""
-
 import PySimpleGUI as sg
 
 layout = [
-            [sg.Text("Название проекта:"), sg.Input( k='expr') ],
-            [sg.Button('Cоздать', size=(54,1)) ]
+            [sg.Text("        Выбор папки:"), sg.In(enable_events=True, key='-FOLDER-'), sg.FolderBrowse()],
+            [sg.Text("Название проекта:"), sg.Input(k='expr')],
+            [sg.Button('Cоздать', size=(55, 1))],
+            [sg.Text("Выбор папок под ваш проект:")],
+            [sg.Checkbox('Creds', default=True, key="-CREDS-"),
+             sg.Checkbox('Images', default=True, key="-IMAGES-"),
+             sg.Checkbox('Videos', default=True, key="-VIDEOS-"),
+             sg.Checkbox('K8s', default=True, key="-K8S-"),
+             sg.Checkbox('Terraform', default=True, key="-TERRAFORM-"),
+             sg.Checkbox('CDK', default=True, key="-CDK-"),
+             sg.Checkbox('Proposal', default=True, key="-PROPOSAL-"),
+             ],
          ]
 
 window = sg.Window("DevOps create project", layout)
-
 while True:
     event, values = window.read()
     print(event, values)
     
     if event == 'Cоздать':
-        #print(expr.va)
         name_proj = values['expr'] # Извлекли название проекта
         print(name_proj)
-        
+        fld = values['-FOLDER-'] # Получаем информацию с блока выбора папки
+        os.chdir(fld) # Меняем директорию для сохранения
         add_folder_in_project(name_proj)
         break
-
-
     if event == sg.WIN_CLOSED:
         break
+
 
 window.close()
          
--- folder_add.py
+++ folder_add.py
@@ -1,65 +1,64 @@
 import os
 
 # Функция проверяет не создана ли уже папка чтобы не бить ошибки
+from DevOps_creator import values
+
+
 def create_dir(dir):
-  if not os.path.exists(dir):
-    os.makedirs(dir)
-    print("Created Directory : ", dir)
-  else:
-    print("Directory already existed : ", dir)
-  return dir
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+        print("Created Directory : ", dir)
+    else:
+        print("Directory already existed : ", dir)
+    return dir
+
+
 # Функция проверяет не создана ли уже папка чтобы не бить ошибки
 
 name = 'proj'
 
+
 def add_folder_in_project(main_folder_name):
     create_dir(main_folder_name)
 
-    root_path =  main_folder_name + '/'
+    root_path = main_folder_name + '/'
 
-    create_dir(root_path+"Creds")
-    f= open(root_path+"Creds/AWS.txt","x")
-    f.write("My AWS account in project\n")
-    f.close()
+    if export_values["-CREDS-"] == True:
+        create_dir(root_path + "Creds")
+        f = open(root_path + "Creds/AWS.txt", "x")
+        f.write("My AWS account in project\n")
+        f.close()
+        create_dir(root_path + "Creds/Db_Creds")
+        f = open(root_path + "Creds/Db_Creds/Db_creds.txt", "x")
+        f.write("Creds DB Dev:\n\n\n")
+        f.write("Creds DB Qa:\n\n\n")
+        f.write("Creds DB Prod:\n\n\n")
+        f.close()
 
-    create_dir(root_path+"Creds/Db_Creds")
-    f= open(root_path+"Creds/Db_Creds/Db_creds.txt","x")
-    f.write("Creds DB Dev:\n\n\n")
-    f.write("Creds DB Qa:\n\n\n")
-    f.write("Creds DB Prod:\n\n\n")
-    f.close()
+    if export_values["-IMAGES-"] == True:
+        create_dir(root_path + "Images")
+        create_dir(root_path + "Images/Diagrams")
+        create_dir(root_path + "Images/Screens")
+        create_dir(root_path + "Images/Other")
 
-    create_dir(root_path+"Videos")
-    create_dir(root_path+"Videos/Backend_side")
-    create_dir(root_path+"Videos/Front_end_side")
-    create_dir(root_path+"Videos/Other")
+    if export_values["-VIDEOS-"] == True:
+        create_dir(root_path + "Videos")
+        create_dir(root_path + "Videos/Backend_side")
+        create_dir(root_path + "Videos/Front_end_side")
+        create_dir(root_path + "Videos/Other")
 
-    create_dir(root_path+"K8s")
-    create_dir(root_path+"Terraform")
-    create_dir(root_path+"Proposal")
-    f= open(root_path+"Proposal/Description.txt","x")
-    f.write("Description:\n")
-    f.close()
-    
+    if export_values["-K8S-"] == True:
+        create_dir(root_path + "K8s")
 
-#add_folder_in_project(name)
-    
+    if export_values["-TERRAFORM-"] == True:
+        create_dir(root_path + "Terraform")
 
+    if export_values["-CDK-"] == True:
+        create_dir(root_path + "CDK")
 
+    if export_values["-PROPOSAL-"] == True:
+        create_dir(root_path + "Proposal")
+        f = open(root_path + "Proposal/Description.txt", "x")
+        f.write("Description:\n")
+        f.close()
 
-
-
-
-
-
-
-#create_dir("Creds")
-#f= open("Creds/aws.txt","w+")
-
-#create_dir("Creds/Db_Creds")
-#f= open("Creds/Db_Creds/Db_creds.txt","w+")
-
-#create_dir("Videos")
-#create_dir("K8s")
-#create_dir("Terraform")
-#create_dir("Proposal")
