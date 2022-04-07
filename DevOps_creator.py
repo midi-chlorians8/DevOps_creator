4dded2fc80eed70f67f7853ac7e168c57b0f3c0d
Shutlersss <denis.korol.001@gmial.com>
2022-04-07 04:21:58 +0300

    added Activated 'Enter' keybord key

--- DevOps_creator.py
+++ DevOps_creator.py
@@ -2,35 +2,36 @@ from folder_add import *
 import PySimpleGUI as sg
 
 layout = [
-            [sg.Text("        Выбор папки:"), sg.In(enable_events=True, key='-FOLDER-'), sg.FolderBrowse()],
-            [sg.Text("Название проекта:"), sg.Input(k='expr')],
-            [sg.Button('Cоздать', size=(55, 1))],
-            [sg.Text("Выбор папок под ваш проект:")],
-            [sg.Checkbox('Creds', default=True, key="-CREDS-"),
-             sg.Checkbox('Images', default=True, key="-IMAGES-"),
-             sg.Checkbox('Videos', default=True, key="-VIDEOS-"),
-             sg.Checkbox('K8s', default=True, key="-K8S-"),
-             sg.Checkbox('Terraform', default=True, key="-TERRAFORM-"),
-             sg.Checkbox('CDK', default=True, key="-CDK-"),
-             sg.Checkbox('Proposal', default=True, key="-PROPOSAL-"),
-             ],
-         ]
+    [sg.Text("        Выбор папки:"), sg.In(enable_events=True, key='-FOLDER-'), sg.FolderBrowse()],
+    [sg.Text("Название проекта:"), sg.Input(k='expr')],
+    [sg.Button('Cоздать', size=(55, 1))],
+    [sg.Text("Выбор папок под ваш проект:")],
+    [sg.Checkbox('Creds', default=True, key="-CREDS-"),
+     sg.Checkbox('Images', default=True, key="-IMAGES-"),
+     sg.Checkbox('Videos', default=True, key="-VIDEOS-"),
+     sg.Checkbox('K8s', default=True, key="-K8S-"),
+     sg.Checkbox('Terraform', default=True, key="-TERRAFORM-"),
+     sg.Checkbox('CDK', default=True, key="-CDK-"),
+     sg.Checkbox('Proposal', default=True, key="-PROPOSAL-"),
+     ],
+]
 
-window = sg.Window("DevOps create project", layout)
+window = sg.Window("DevOps create project", layout, finalize=True)
+window['expr'].bind("<Return>", "_Enter")
+
 while True:
     event, values = window.read()
     print(event, values)
-    
-    if event == 'Cоздать':
-        name_proj = values['expr'] # Извлекли название проекта
+
+    if event == 'Cоздать' or event == "expr_Enter":
+        name_proj = values['expr']  # Извлекли название проекта
         print(name_proj)
-        fld = values['-FOLDER-'] # Получаем информацию с блока выбора папки
-        os.chdir(fld) # Меняем директорию для сохранения
+        fld = values['-FOLDER-']  # Получаем информацию с блока выбора папки
+        if fld != '':
+            os.chdir(fld)  # Меняем директорию для сохранения
         add_folder_in_project(name_proj, values)
         break
     if event == sg.WIN_CLOSED:
         break
 
-
 window.close()
-         
