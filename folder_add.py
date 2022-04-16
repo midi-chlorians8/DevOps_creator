028248ab4ed65619d3570d9d0c010c927e6ba23d
midi-chlorians8 <50805334+midi-chlorians8@users.noreply.github.com>
2022-04-16 13:45:25 +0300

    Update folder_add.py

--- folder_add.py
+++ folder_add.py
@@ -19,11 +19,12 @@ def add_folder_in_project(main_folder_name, values):
 
     if values['-CREDS-'] == True:
         create_dir(root_path + "Creds")
-        f = open(root_path + "Creds/AWS.txt", "x")
+        f = open(root_path + "Creds/"+main_folder_name+"_"+"AWS.txt", "x")
         f.write("My AWS account in project\n")
         f.close()
         create_dir(root_path + "Creds/Db_Creds")
-        f = open(root_path + "Creds/Db_Creds/Db_creds.txt", "x")
+
+        f = open(root_path + "Creds/Db_Creds/"+main_folder_name+"_"+"Db_creds.txt", "x")
         f.write("Creds DB Dev:\n\n\n")
         f.write("Creds DB Qa:\n\n\n")
         f.write("Creds DB Prod:\n\n\n")
@@ -48,6 +49,26 @@ def add_folder_in_project(main_folder_name, values):
         create_dir(root_path + "Terraform")
         create_dir(root_path + "Terraform/Polygon")
         create_dir(root_path + "Terraform/Work_Version")
+
+    if values["-MONITORINGS-"] == True:
+        create_dir(root_path + "Monitorings")
+        f = open(root_path + "Monitorings/"+main_folder_name+"_"+"Creds_Monitorings.txt", "x")
+        f.write("My Monitorings in project creds:\n\n")
+        f.write("Creds Grafana:\n")
+
+        f.write("Dev:\n")
+        f.write("Login:\n")
+        f.write("Password:\n\n")
+
+        f.write("Qa:\n")
+        f.write("Login:\n")
+        f.write("Password:\n\n")
+
+        f.write("Prod:\n")
+        f.write("Login:\n")
+        f.write("Password:\n\n")
+
+        f.close()
 
     if values["-CDK-"] == True:
         create_dir(root_path + "CDK")
