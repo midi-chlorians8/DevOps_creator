2d30b635e1ee645deaaf1c4d518ada70a2bfc368
midi-chlorians8 <50805334+midi-chlorians8@users.noreply.github.com>
2022-04-16 13:45:08 +0300

    Update DevOps_creator.py

--- DevOps_creator.py
+++ DevOps_creator.py
@@ -2,17 +2,19 @@ from folder_add import *
 import PySimpleGUI as sg
 
 layout = [
-    [sg.Text("        Выбор папки:"), sg.In(enable_events=True, key='-FOLDER-'), sg.FolderBrowse()],
     [sg.Text("Название проекта:"), sg.Input(k='expr')],
+    [sg.Text("        Выбор папки:"), sg.In(enable_events=True, key='-FOLDER-'), sg.FolderBrowse()],
     [sg.Button('Cоздать', size=(55, 1))],
     [sg.Text("Выбор папок под ваш проект:")],
     [sg.Checkbox('Creds', default=True, key="-CREDS-"),
-     sg.Checkbox('Images', default=True, key="-IMAGES-"),
-     sg.Checkbox('Videos', default=True, key="-VIDEOS-"),
      sg.Checkbox('K8s', default=True, key="-K8S-"),
      sg.Checkbox('Terraform', default=True, key="-TERRAFORM-"),
-     sg.Checkbox('CDK', default=True, key="-CDK-"),
+     sg.Checkbox('Monitorings', default=True, key="-MONITORINGS-"),
      sg.Checkbox('Proposal', default=True, key="-PROPOSAL-"),
+     sg.Checkbox('Images', default=True, key="-IMAGES-"),
+     sg.Checkbox('Videos', default=True, key="-VIDEOS-"),
+     sg.Checkbox('CDK', default=True, key="-CDK-"),
+
      ],
 ]
 
