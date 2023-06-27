5dd21b40aab1ee0ca028a72d3823c12d5582fb05
iu <ilya.romanovskiy.2015@mail.ru>
2023-06-28 01:16:22 +0300

    add pic to front

--- DevOps_creator.py
+++ DevOps_creator.py
@@ -3,6 +3,8 @@ import PySimpleGUI as sg
 
 sg.theme('DarkGrey13')   # Keep things interesting for your users
 
+background_layout = [[sg.Image(r'pic_front.png')]]
+
 layout = [
     [sg.Text("Название проекта:"), sg.Input(k='expr')],
     [sg.Text("        Выбор папки:"), sg.In(enable_events=True, key='-FOLDER-'), sg.FolderBrowse()],
@@ -18,9 +20,10 @@ layout = [
      sg.Checkbox('Images', default=False, key="-IMAGES-"),
      sg.Checkbox('Videos', default=False, key="-VIDEOS-"),
      #  sg.Checkbox('CDK', default=False, key="-CDK-"),
-     
-     ],
+    ],
 ]
+
+layout = background_layout + layout
 
 window = sg.Window("DevOps create project", layout, finalize=True)
 window['expr'].bind("<Return>", "_Enter")
